b2e070627e3aa97892854fb5a4b10810513c2c34
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-02-19 19:51:36 -0800

    Showing generation using hooks

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Grid() {
+  return "Grid";
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Main() {
+  const [generation, setGeneration] = useState(0);
+
+  return (
+    <div>
+      <h1>Game of Life</h1>
+      <Grid />
+      <h2>Generations: {generation}</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
